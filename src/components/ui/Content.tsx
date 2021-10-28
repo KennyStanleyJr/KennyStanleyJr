40089319513cf991da21948a02f45b581f34657c
Kenny Stanley <kpstanleyjr@gmail.com>
2021-10-28 17:46:03 -0500

    Simplified components

---
+++ src/components/ui/Content.tsx
@@ -0,0 +1,9 @@
+export const Content = () => {
+  return (
+    <h2 className="text-2xl text-white max-w-md mx-auto" data-testid="content">
+      A simple Next.js application with TypeScript, Tailwind CSS and testing
+      suite enabled
+    </h2>
+  )
+}
+export default Content

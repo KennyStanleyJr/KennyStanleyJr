ac9eb31645a299ca7270436201fde2118905ba70
Kenny Stanley <kpstanleyjr@gmail.com>
2021-10-28 22:24:03 -0500

    Added theme icons

--- src/components/ui/DarkModeToggle.tsx
+++ src/components/ui/DarkModeToggle.tsx
@@ -13,8 +13,42 @@ export const DarkModeToggle = () => {
   return (
     <>
       <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
-        <div className="bg-black dark:bg-white text-white dark:text-black border-black border-2 rounded-2xl w-10 h-10">
-          {theme}
+        <div
+          className="p-2 bg-black dark:bg-white text-white dark:text-black 
+                      border-black border-2 rounded-3xl hover:rounded-lg shadow-xl
+                      transition-all ease-linear cursor-pointer"
+        >
+          {theme === 'light' ? (
+            <svg
+              className="w-8 h-8"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+              />
+            </svg>
+          ) : (
+            <svg
+              className="w-8 h-8"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+              />
+            </svg>
+          )}
         </div>
       </button>
     </>

1eb0d84fd9c4f8c9c01f4093269309cfab1aaee1
Kenny Stanley <kpstanleyjr@gmail.com>
2021-10-28 23:30:10 -0500

    added border on hover

--- src/components/ui/NavItem.tsx
+++ src/components/ui/NavItem.tsx
@@ -2,8 +2,10 @@ import Link from 'next/link'
 
 export const NavItem = ({ pageName }: { pageName: string }) => {
   return (
-    <li className="dark:text-white transition-all">
-      <Link href={`/${pageName}`}>{pageName}</Link>
+    <li className="dark:text-white transition-all border-2 border-transparent hover:border-gray-200 rounded-2xl">
+      <Link href={`/${pageName}`} passHref>
+        <p className="px-4 py-2 cursor-pointer">{pageName}</p>
+      </Link>
     </li>
   )
 }

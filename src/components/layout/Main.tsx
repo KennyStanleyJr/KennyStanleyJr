6dccf59c66bc8a4f840524365d964defa965e49f
Kenny Stanley <kpstanleyjr@gmail.com>
2021-10-28 22:34:24 -0500

    Removed unused code

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,10 +1,6 @@
-import { useTheme } from 'next-themes'
-
 export default function Main({ children }: { children: React.ReactNode }) {
-  const { theme, setTheme } = useTheme()
-
   return (
-    <div className="bg-gray-200 dark:bg-almost-black">
+    <div className="bg-gray-200 dark:bg-almost-black transition-colors">
       <main className="h-screen max-w-7xl mx-auto sm:px-6 lg:px-8">
         {children}
       </main>

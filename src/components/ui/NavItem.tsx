a8b7cc4e58e0aa68ed05aad84d26f29e0a38e928
Kenny Stanley <kpstanleyjr@gmail.com>
2021-10-28 22:24:28 -0500

    fixed text color in light mode

--- src/components/ui/NavItem.tsx
+++ src/components/ui/NavItem.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export const NavItem = ({ pageName }: { pageName: string }) => {
   return (
-    <li className="text-white">
+    <li className="dark:text-white transition-all">
       <Link href={`/${pageName}`}>{pageName}</Link>
     </li>
   )

12d3a4de6f57696ce5df611bfc2beadde27e6009
Kenny Stanley <kpstanleyjr@gmail.com>
2021-10-28 23:33:23 -0500

    reverted to fix bg-color

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,10 +1,9 @@
 export default function Main({ children }: { children: React.ReactNode }) {
   return (
-    <main
-      className="h-screen max-w-7xl mx-auto sm:px-6 lg:px-8
-      bg-gray-200 dark:bg-almost-black font-poppins transition-colors"
-    >
-      {children}
-    </main>
+    <div className="bg-gray-200 dark:bg-almost-black font-poppins transition-colors">
+      <main className="h-screen max-w-7xl mx-auto sm:px-6 lg:px-8">
+        {children}
+      </main>
+    </div>
   )
 }

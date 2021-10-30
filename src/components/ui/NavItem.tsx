eeff1eb0d406f61ea02a6c1be8b66949604ca57d
Kenny Stanley <kpstanleyjr@gmail.com>
2021-10-29 21:01:43 -0500

    fixed border for light mode

--- src/components/ui/NavItem.tsx
+++ src/components/ui/NavItem.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export const NavItem = ({ pageName }: { pageName: string }) => {
   return (
-    <li className="dark:text-white transition-all border-2 border-transparent hover:border-gray-200 rounded-2xl">
+    <li className="text-almost-black dark:text-white transition-all border-2 border-transparent hover:border-almost-black dark:hover:border-gray-200  rounded-2xl">
       <Link href={`/${pageName}`} passHref>
         <p className="px-4 py-2 cursor-pointer">{pageName}</p>
       </Link>

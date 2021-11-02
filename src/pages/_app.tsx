727820864a576ef5ce21f7b69d11c7f659c1424b
Kenny Stanley <kpstanleyjr@gmail.com>
2021-11-01 23:05:27 -0500

    moved canvas to _app.tsx for persistence

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,38 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import { ThemeProvider } from 'next-themes'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Stars } from '@react-three/drei'
+import { useRef } from 'react'
+
+const RotateStars = ({ starsRef }: any) => {
+  useFrame(() => {
+    // Rotate the model a bit
+    if (starsRef.current) {
+      starsRef.current.rotation.z = starsRef.current.rotation.y += 0.0002
+    }
+  })
+  return <></>
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const starsRef = useRef()
   return (
     <ThemeProvider attribute="class" defaultTheme="system">
+      <div className="h-screen">
+        <Canvas>
+          <Stars
+            ref={starsRef}
+            radius={100}
+            depth={50}
+            count={5000}
+            factor={4}
+            saturation={0}
+            fade
+          />
+          <RotateStars starsRef={starsRef} />
+        </Canvas>
+      </div>
       <Component {...pageProps} />
     </ThemeProvider>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,16 @@
-import { useRef } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { Html, OrbitControls, Stars } from '@react-three/drei'
-
 import Main from '@/components/layout/Main'
 import Header from '@/components/layout/Header'
 import Logo from '@/components/ui/Logo'
 import Navigation from '@/components/domain/Navigation'
 import DarkModeToggle from '@/components/ui/DarkModeToggle'
 import Intro from '@/components/ui/Intro'
-import HeroModel from '@/components/ui/HeroModel'
 import Container from '@/components/layout/Container'
 import HeroSection from '@/components/layout/HeroSection'
 
-const Screen = ({ starsRef }: any) => {
-  useFrame(() => {
-    // Rotate the model a bit
-    starsRef.current.rotation.z = starsRef.current.rotation.y += 0.0002
-  })
+export default function Home() {
   return (
     <>
-      <Html fullscreen>
+      <div className="absolute top-0 left-0 w-full h-full">
         <Main>
           <Header>
             <Logo />
@@ -34,31 +25,6 @@ const Screen = ({ starsRef }: any) => {
             </HeroSection>
           </Container>
         </Main>
-      </Html>
-    </>
-  )
-}
-
-export default function Home() {
-  const starsRef = useRef()
-
-  return (
-    <>
-      <div className="h-screen">
-        <Canvas>
-          <Screen starsRef={starsRef} />
-          <Stars
-            ref={starsRef}
-            radius={100}
-            depth={50}
-            count={5000}
-            factor={4}
-            saturation={0}
-            fade
-          />
-
-          <OrbitControls />
-        </Canvas>
       </div>
     </>
   )

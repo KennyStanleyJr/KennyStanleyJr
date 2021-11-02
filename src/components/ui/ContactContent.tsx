80e09b5515e30bc88811f93b1513851c72be28b1
Kenny Stanley <kpstanleyjr@gmail.com>
2021-11-01 23:06:31 -0500

    simple contact page

---
+++ src/components/ui/ContactContent.tsx
@@ -0,0 +1,8 @@
+export const ContactContent = () => {
+  return (
+    <div className="flex flex-col h-full justify-center">
+      <h3 className="text-3xl font-light">Contact</h3>
+    </div>
+  )
+}
+export default ContactContent

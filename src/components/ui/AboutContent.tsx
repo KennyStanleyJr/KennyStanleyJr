107afb1934867d7d0b81d72656b3284f187465bb
Kenny Stanley <kpstanleyjr@gmail.com>
2021-11-01 23:05:52 -0500

    simple about page

---
+++ src/components/ui/AboutContent.tsx
@@ -0,0 +1,8 @@
+export const AboutContent = () => {
+  return (
+    <div className="flex flex-col h-full justify-center">
+      <h3 className="text-3xl font-light">About</h3>
+    </div>
+  )
+}
+export default AboutContent

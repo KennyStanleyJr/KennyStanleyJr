7f22d403afd551bebb52dc7770ff0a83fb5735de
Kenny Stanley <kpstanleyjr@gmail.com>
2021-11-01 22:13:37 -0500

    Added rotating stars background

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,6 +1,6 @@
 export default function Main({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-gray-200 dark:bg-almost-black font-poppins transition-colors">
+    <div className="font-poppins transition-colors">
       <main className="flex flex-col h-screen">{children}</main>
     </div>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
+import { useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Html, OrbitControls, Stars } from '@react-three/drei'
+
 import Main from '@/components/layout/Main'
 import Header from '@/components/layout/Header'
 import Logo from '@/components/ui/Logo'
@@ -5,32 +9,57 @@ import Navigation from '@/components/domain/Navigation'
 import DarkModeToggle from '@/components/ui/DarkModeToggle'
 import Intro from '@/components/ui/Intro'
 import HeroModel from '@/components/ui/HeroModel'
-import { Canvas } from '@react-three/fiber'
 import Container from '@/components/layout/Container'
 import HeroSection from '@/components/layout/HeroSection'
 
-export default function Home() {
+const Screen = ({ starsRef }: any) => {
+  useFrame(() => {
+    // Rotate the model a bit
+    starsRef.current.rotation.z = starsRef.current.rotation.y += 0.0002
+  })
   return (
     <>
-      <Main>
-        <Header>
-          <Logo />
-          <div className="hidden md:block">
-            <Navigation />
-          </div>
-          <DarkModeToggle />
-        </Header>
-        <Container>
-          <HeroSection>
-            <Intro />
-            <div className="flex-1">
-              <Canvas>
-                <HeroModel />
-              </Canvas>
+      <Html fullscreen>
+        <Main>
+          <Header>
+            <Logo />
+            <div className="hidden md:block">
+              <Navigation />
             </div>
-          </HeroSection>
-        </Container>
-      </Main>
+            <DarkModeToggle />
+          </Header>
+          <Container>
+            <HeroSection>
+              <Intro />
+            </HeroSection>
+          </Container>
+        </Main>
+      </Html>
+    </>
+  )
+}
+
+export default function Home() {
+  const starsRef = useRef()
+
+  return (
+    <>
+      <div className="h-screen">
+        <Canvas>
+          <Screen starsRef={starsRef} />
+          <Stars
+            ref={starsRef}
+            radius={100}
+            depth={50}
+            count={5000}
+            factor={4}
+            saturation={0}
+            fade
+          />
+
+          <OrbitControls />
+        </Canvas>
+      </div>
     </>
   )
 }

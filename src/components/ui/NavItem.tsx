2ecafd1dc79e241589c858148e283543f3ad1e95
Kenny Stanley <kpstanleyjr@gmail.com>
2021-11-03 17:35:58 -0500

    active page is now highlighted in nav

--- src/components/ui/NavItem.tsx
+++ src/components/ui/NavItem.tsx
@@ -1,8 +1,15 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export const NavItem = ({ pageName }: { pageName: string }) => {
+  const router = useRouter()
+  const isActive = router.pathname === `/${pageName}`
   return (
-    <li className="text-almost-black dark:text-white transition-all border-2 border-transparent hover:border-almost-black dark:hover:border-gray-200  rounded-2xl">
+    <li
+      className={`text-almost-black dark:text-white transition-all border-2 border-transparent hover:border-almost-black dark:hover:border-gray-200 rounded-2xl ${
+        isActive && 'border-black dark:border-white'
+      }`}
+    >
       <Link href={`/${pageName}`} passHref>
         <p className="px-4 py-2 cursor-pointer">{pageName}</p>
       </Link>

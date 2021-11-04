39312964c6339531024a022650a1f864fea563ff
Kenny Stanley <kpstanleyjr@gmail.com>
2021-11-04 12:28:01 -0500

    added bottom spacer for mobile

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -2,6 +2,7 @@ export default function Main({ children }: { children: React.ReactNode }) {
   return (
     <div className="font-poppins transition-colors">
       <main className="flex flex-col h-screen">{children}</main>
+      <div className="h-20 bg-transparent md:hidden"></div>
     </div>
   )
 }

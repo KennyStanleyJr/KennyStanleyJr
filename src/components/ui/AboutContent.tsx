b19e4bcbfc1727648175f1efd87b26f7f66127d6
Kenny Stanley <kpstanleyjr@gmail.com>
2021-11-04 12:22:09 -0500

    added content to about page

--- src/components/ui/AboutContent.tsx
+++ src/components/ui/AboutContent.tsx
@@ -1,7 +1,28 @@
+import Image from 'next/image'
+
 export const AboutContent = () => {
   return (
     <div className="flex flex-col h-full justify-center">
-      <h3 className="text-3xl font-light">About</h3>
+      <h3 className="text-5xl font-bold text-center mb-12">About</h3>
+      <div className="flex flex-col md:flex-row gap-8">
+        <div className="text-xl font-light">
+          <p>Hello, my name is Kenny Stanley.</p>
+          <p>I am passionate about solving problems with technology.</p>
+          <p>
+            Graduated from Tennessee Technological University with a Bachelors
+            in Electrical Engineering.
+          </p>
+        </div>
+
+        <div className="w-52 h-52 md:w-72 md:h-72 relative mx-auto">
+          <Image
+            src="/d1.jpg"
+            alt="Picture of me, Kenny Stanley"
+            layout="fill"
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
     </div>
   )
 }

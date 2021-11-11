167e1f586fbffdd63cb0e2b8a4274c4ed1c12dff
Kenny Stanley <kpstanleyjr@gmail.com>
2021-11-10 18:08:11 -0600

    made contact page responsive

--- src/components/ContactContent.tsx
+++ src/components/ContactContent.tsx
@@ -6,7 +6,7 @@ export const ContactContent = () => {
       <h3 className="text-4xl md:text-5xl font-bold text-center mb-12">
         Contact
       </h3>
-      <div className="flex flex-col md:flex-row gap-48">
+      <div className="flex flex-col md:flex-row gap-12 lg:gap-48">
         <ContactDetails />
         <ContactForm />
       </div>
@@ -18,8 +18,14 @@ export default ContactContent
 const ContactDetails = () => {
   return (
     <>
-      <div className="flex flex-col gap-6">
-        <h1>Nashville, TN</h1>
+      <div className="flex flex-col gap-6 items-baseline">
+        <h1 className="text-xl md:text-3xl">
+          <span className="text-blue-600">Nashville</span>, TN
+        </h1>
+        <div className="flex flex-col gap-4 text-md md:text-xl">
+          <a href="mailto:kpstanleyjr@gmail.com">kpstanleyjr@gmail.com</a>
+          <a href="tel:+1-615-498-4465">615-498-4465</a>
+        </div>
       </div>
     </>
   )
@@ -49,15 +55,18 @@ const ContactForm = () => {
   )
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-6 text-xl">
-      <div className="flex flex-col md:flex-row gap-6">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-4 md:gap-6 text-md md:text-lg lg:text-xl"
+    >
+      <div className="flex flex-col md:flex-row gap-4 md:gap-6">
         <input
           id="name"
           type="text"
           placeholder="Name"
           value={details.name}
           onChange={handleChange}
-          className="p-4 rounded"
+          className="p-2 md:p-3 lg:p-4 rounded border-2 dark:border-transparent"
         />
         <input
           id="email"
@@ -65,7 +74,7 @@ const ContactForm = () => {
           placeholder="Email"
           value={details.email}
           onChange={handleChange}
-          className="p-4 rounded"
+          className="p-2 md:p-3 lg:p-4 rounded border dark:border-transparent"
         />
       </div>
 
@@ -75,18 +84,18 @@ const ContactForm = () => {
         placeholder="Subject (Optional)"
         value={details.subject}
         onChange={handleChange}
-        className="p-4 rounded"
+        className="p-2 md:p-3 lg:p-4 rounded border dark:border-transparent"
       />
       <textarea
         id="message"
         placeholder="Message"
         value={details.message}
         onChange={handleChange}
-        className="p-4 rounded"
+        className="p-2 md:p-3 lg:p-4 rounded border dark:border-transparent"
       />
       <button
         type="submit"
-        className="border-2 border-blue-600 hover:bg-blue-600 p-4 rounded transition-all duration-200"
+        className="border-2 border-blue-600 hover:bg-blue-600 p-2 md:p-3 lg:p-4 rounded transition-all duration-200"
       >
         Send Message
       </button>

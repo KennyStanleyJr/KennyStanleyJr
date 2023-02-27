00b41efaec2043f21f1f1a49ad0e1a51bd3869b1
Kenny Stanley <kpstanleyjr@gmail.com>
2023-02-27 13:35:24 -0600

    Updated logo to permanent marker font

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useTheme } from "next-themes";
 
 export function Logo() {
+    const { theme } = useTheme()
+
     return (
         <Link href="/" passHref>
             <Image
-                src="/logo.svg"
+                src={theme === 'dark' ? "/logo-white.png" : "/logo-black.png"}
                 width={80}
                 height={80}
                 alt="logo image"

689fbe8c378392811456e2a380cb96f7c976bb2d
Kenny Stanley <kpstanleyjr@gmail.com>
2023-02-26 20:25:56 -0600

    Added animated border radius to nav links to match dark mode toggle button

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -11,7 +11,7 @@ export const NavItem = ({ pageName }: { pageName: string }) => {
   const isActive = router.pathname === pageLink
   return (
     <li
-      className={`text-almost-black dark:text-white transition-all duration-300 border-2 border-transparent hover:border-almost-black hover:bg-almost-black hover:text-white dark:hover:bg-white dark:hover:text-black dark:hover:border-white rounded-2xl ${
+      className={`text-almost-black dark:text-white transition-all duration-300 border-2 border-transparent hover:border-almost-black hover:bg-almost-black hover:text-white dark:hover:bg-white dark:hover:text-black dark:hover:border-white animated-radius ${
         isActive && 'border-black dark:border-white'
       }`}
     >

f3041d2f6f8fecc23e3a18a51a4482ddd3e9e09a
Kenny Stanley <kpstanleyjr@gmail.com>
2023-02-27 13:11:37 -0600

    Updated picture of myself and added bottom border with shadow

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -14,13 +14,16 @@ export function AboutSection() {
                     <p>No, like really, I promise.</p>
                 </div>
 
-                <div className="w-52 h-52 md:w-72 md:h-72 relative mx-auto">
+                <div className="relative w-52 h-52 md:w-72 md:h-72 mx-auto">
                     <Image
-                        src="/d1.jpg"
+                        src="/kenny.png"
                         alt="Picture of me, Kenny Stanley"
                         fill
                         className="rounded-xl object-cover"
+                        priority
                     />
+                    <div id="bottomBorder" className="absolute bottom-0 bg-neutral-300 dark:bg-neutral-800 h-0.5 w-full rounded-full" />
+                    <div id="bottomBorderShadow" className="absolute -bottom-2 bg-black dark:bg-neutral-500 h-0.5 w-full blur-[10px] rounded-full" />
                 </div>
             </div>
         </div>

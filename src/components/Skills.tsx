efa0b9a48832204753127de99d1ce3db5ee6e9c9
Kenny Stanley <kpstanleyjr@gmail.com>
2023-02-28 13:34:51 -0600

    Added grid styling to skills section to conserve space and look better

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -8,26 +8,30 @@ export type Skill = {
 
 export function Skills({ skills }: { skills: Skill[] }) {
     return <>
-        <h1 className="text-4xl md:text-5xl font-bold text-center mb-2">Skills</h1>
+        <h1 className="text-3xl md:text-4xl text-center mb-2">Skills</h1>
 
+        <div className="h-1 w-32 bg-neutral-200 dark:bg-neutral-800 rounded-full mx-auto mb-4"></div>
+
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-2 md:gap-4">
         <Category category="languages" skills={skills} />
         <Category category="front-end" skills={skills} />
         <Category category="back-end" skills={skills} />
         <Category category="tools" skills={skills} />
         <Category category="other" skills={skills} />
+        </div>
 
     </>
 }
 
 function Category({ category, skills }: { category: Category, skills: Skill[] }) {
-    return <>
+    return <div>
         <h3 className="mt-1 text-md md:text-lg lg:text-xl capitalize">{category}</h3>
         <div className="flex gap-1 flex-wrap">
             {skills.filter(skill => skill.category === category).map(skill => <Skill key={skill.name} {...skill} />)}
         </div>
-    </>
+    </div>
 }
 
 function Skill({ name, icon, category }: Skill) {
-    return <div className="text-xs md:text-sm bg-black text-white dark:bg-white dark:text-black rounded px-2 py-1">{name}</div>
+    return <div className="bg-neutral-200 dark:bg-neutral-800 text-neutral-800 dark:text-neutral-200 text-xs px-2 py-1 rounded-md">{name}</div>
 }
